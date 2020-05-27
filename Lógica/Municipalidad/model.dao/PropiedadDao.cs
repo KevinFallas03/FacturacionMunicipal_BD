b41a2a584412eeff400c423ec292616143b40055
KevinFallas03 <48746116+KevinFallas03@users.noreply.github.com>
2020-05-27 00:23:34 -0600

    Clases Propiedad y PropietadadDao

---
+++ Lógica/Municipalidad/model.dao/PropiedadDao.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using model.entity;
+
+namespace model.dao
+{
+    public class PropiedadDao : TemplateCRUD<Propiedad>
+    {
+        public void create(Propiedad objetoPropiedad)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void delete(Propiedad objetoPropiedad)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool find(Propiedad objetoPropiedad)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Propiedad> findAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void update(Propiedad objetoPropiedad)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Lógica/Municipalidad/model.entity/Propiedad.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace model.entity
+{
+    public class Propiedad
+    {
+        private int ID; 
+        private int numeroPropiedad;
+        private Decimal valorPropiedad;
+        private string direccionPropiedad;
+
+        public Propiedad() {}
+        public Propiedad(int pID)
+        {
+            this.ID = pID;
+        }
+        public Propiedad(int pNumeroPropiedad, Decimal pValorPropiedad, string pDireccionPropiedad)
+        {
+            this.NumeroPropiedad = pNumeroPropiedad;
+            this.ValorPropiedad = pValorPropiedad;
+            this.DireccionPropiedad = pDireccionPropiedad;
+        }
+
+        //Getters and Setters
+
+        public int NumeroPropiedad { get => numeroPropiedad; set => numeroPropiedad = value; }
+        public Decimal ValorPropiedad { get => valorPropiedad; set => valorPropiedad = value; }
+        public string DireccionPropiedad { get => direccionPropiedad; set => direccionPropiedad = value; }
+    }
+}
