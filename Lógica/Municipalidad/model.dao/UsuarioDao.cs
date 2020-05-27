538b1c9ee8764fb2b6d50b702e382efb2a5e6df8
KevinFallas03 <48746116+KevinFallas03@users.noreply.github.com>
2020-05-27 00:23:08 -0600

    Creacion de clases Usuario y UsuarioDao

---
+++ Lógica/Municipalidad/model.dao/UsuarioDao.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using model.entity;
+
+namespace model.dao
+{
+    public class UsuarioDao : TemplateCRUD<Usuario>
+    {
+        public void create(Usuario objetoUsuario)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void delete(Usuario objetoUsuario)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool find(Usuario objetoUsuario)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Usuario> findAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void update(Usuario objetoUsuario)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Lógica/Municipalidad/model.entity/Usuario.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace model.entity
+{
+    public class Usuario
+    {
+        private int ID;
+        private string nombreUsuario;
+        private string password;
+        private string tipoUsuario;
+        private Usuario() {}
+        private Usuario(int pID) 
+        {
+            this.ID = pID;
+        }
+        private Usuario(string pNombreUsuario, string pPassword, string pTipoUsuario)
+        {
+            this.NombreUsuario = pNombreUsuario;
+            this.Password = pPassword;
+            this.TipoUsuario = pTipoUsuario;
+        }
+
+        //getters and setters
+        public string NombreUsuario { get => nombreUsuario; set => nombreUsuario = value; }
+        public string Password { get => password; set => password = value; }
+        public string TipoUsuario { get => tipoUsuario; set => tipoUsuario = value; }
+    }
+}
