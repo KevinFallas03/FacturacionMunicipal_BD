f094b1c29311c428befe224820ade86437a5abd0
KevinFallas03 <48746116+KevinFallas03@users.noreply.github.com>
2020-05-28 15:41:16 -0600

    Nueva ruta para el Usuario

--- Lógica/Municipalidad/MunicipalidadWebMVC5/App_Start/RouteConfig.cs
+++ Lógica/Municipalidad/MunicipalidadWebMVC5/App_Start/RouteConfig.cs
@@ -14,10 +14,18 @@ namespace MunicipalidadWebMVC5
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "Usuario",
+                url: "usuario/listado",
+                defaults: new { controller = "Usuario", action = "Inicio", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Propietario", action = "Inicio", id = UrlParameter.Optional }
             );
+
+            
         }
     }
 }
