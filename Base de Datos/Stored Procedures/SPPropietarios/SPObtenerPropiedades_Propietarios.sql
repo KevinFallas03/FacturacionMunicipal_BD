e04971a4bc2817b5848b91512894d2723b862499
KevinFallas03 <48746116+KevinFallas03@users.noreply.github.com>
2020-06-05 10:11:02 -0600

    Cambio en spProp?Prop whew Propietario.Id

--- Base de Datos/Stored Procedures/SPPropietarios/SPObtenerPropiedades_Propietarios.sql
+++ Base de Datos/Stored Procedures/SPPropietarios/SPObtenerPropiedades_Propietarios.sql
@@ -9,6 +9,6 @@ as
 	FROM Prop_Prop
 	INNER JOIN Propiedad ON Prop_Prop.IdPropiedad=Propiedad.ID
 	INNER JOIN Propietario ON Prop_Prop.IdPropietario=Propietario.ID
-	where Prop_Prop.ID = @id and Prop_Prop.EstaBorrado = 0 and Propietario.EstaBorrado=0 and Propiedad.EstaBorrado=0
+	where Propietario.ID = @id and Prop_Prop.EstaBorrado = 0 and Propietario.EstaBorrado=0 and Propiedad.EstaBorrado=0
 
 go
