c45090e2683bb5852982ee369ec4a44fe893bf88
KevinFallas03 <48746116+KevinFallas03@users.noreply.github.com>
2020-07-07 20:33:58 -0600

    Correcion del SP para insertar usuarios

--- Base de Datos/Stored Procedures/SPUsuarios/SPInsertarUsuario.sql
+++ Base de Datos/Stored Procedures/SPUsuarios/SPInsertarUsuario.sql
@@ -3,13 +3,12 @@ use FacturacionMunicipal
 go
 Create or Alter procedure spInsertarUsuario
 (
-	@ID int,
 	@Nombre VARCHAR(150),
 	@Password VARCHAR(150),
 	@TipoUsuario VARCHAR(150)
 )
 as
 Begin
-	Insert into [dbo].Usuario(ID, Nombre, Password, TipoUsuario, EstaBorrado )
-	Values (@ID, @Nombre, @Password, @TipoUsuario, 0)
+	Insert into [dbo].Usuario(Nombre, Password, TipoUsuario, FechaIngreso, EstaBorrado )
+	Values (@Nombre, @Password, @TipoUsuario, CONVERT(DATE,GETDATE()), 0)
 End
