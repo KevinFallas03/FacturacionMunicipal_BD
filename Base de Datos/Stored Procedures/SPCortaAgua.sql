735ad7e94fa531dd3eeeadd07065d2dec4bdd44d
Heyler-Johel <johelmora@gmail.com>
2020-07-10 17:37:44 -0600

    SP para Corta y Reconexión de Agua.

---
+++ Base de Datos/Stored Procedures/SPCortaAgua.sql
@@ -0,0 +1,60 @@
+USE [FacturacionMunicipal]
+GO
+/****** Object:  StoredProcedure [dbo].[spCortaAgua]    Script Date: 10/7/2020 08:26:57 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE or ALTER PROC [dbo].[spCortaAgua] @FechaActual DATE
+AS  	
+	BEGIN
+		BEGIN TRY
+			DECLARE @idPropiedades TABLE(ID INT IDENTITY(1,1),IdPropiedad int,IdCCobro INT)
+			DECLARE @idMenor INT, @idMayor INT, @id int, @idc int, @cant int
+			BEGIN TRANSACTION
+				INSERT INTO @idPropiedades(IdPropiedad,IdCCobro)
+				SELECT R.IdPropiedad, R.IdCCobro
+				FROM Recibo AS R
+				WHERE R.Estado = 0 AND R.IdCCobro = 1 AND NOT EXISTS (SELECT ID FROM [dbo].[Recibo] WHERE IdCCobro = 10)
+				GROUP BY IdPropiedad, IdCCobro
+				HAVING COUNT(*) > 1
+				 
+				SELECT @idMenor = MIN(ID), @idMayor = MAX(ID) FROM @idPropiedades
+
+				WHILE @idMenor<=@idMayor
+				BEGIN
+					INSERT INTO Recibo (IdCCobro,Monto,Estado,IdPropiedad,FechaEmision,FechaMaximaPago)
+					SELECT 10, CM.MontoFijo, 0, P.idPropiedad, @FechaActual, DATEADD(DAY,CC.QDiasVencimiento,@FechaActual)
+					FROM @idPropiedades AS P
+					INNER JOIN CCobro_MontoFijo AS CM ON CM.ID = 10
+					INNER JOIN CCobro AS CC ON CC.ID = 10
+					WHERE P.id = @idMenor
+
+					SET @id = IDENT_CURRENT('Recibo')
+					INSERT INTO ReciboReconexion(id)
+					SELECT @id
+
+					SELECT @idc = MAX(ID) + 1 FROM Corte
+					SELECT @cant = (COUNT(*)) FROM Corte
+					IF  @cant = 0 
+						BEGIN
+							SELECT @idc=1
+						END
+
+					INSERT INTO Corte(ID, IdPropiedad,IdReciboReconexion,Fecha)
+					SELECT @idc, idp.idPropiedad, @id, @FechaActual
+					FROM @idPropiedades as idP
+					WHERE idP.id =	@idMenor
+					
+					SET @idMenor = @idMenor+1
+				END
+				
+			COMMIT
+		END TRY
+		BEGIN CATCH
+			ROLLBACK TRAN;
+			THROW 554004,'Error: No se ha podido cortar el agua',1;
+		END CATCH
+		
+	END
---
+++ Base de Datos/Stored Procedures/spReconexionAgua.sql
@@ -0,0 +1,49 @@
+USE [FacturacionMunicipal]
+GO
+/****** Object:  StoredProcedure [dbo].[spReconexionAgua]    Script Date: 10/7/2020 08:26:57 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE or ALTER PROC [dbo].[spReconexionAgua] @FechaActual DATE AS  	
+	BEGIN
+		--BEGIN TRY
+			DECLARE @idPropiedades TABLE(id INT IDENTITY(1,1),IdPropiedad int)
+			DECLARE @idMenor INT, @idMayor INT, @idc INT, @cant INT
+			BEGIN TRAN
+				INSERT INTO @idPropiedades(idPropiedad)
+				SELECT DISTINCT R.IdPropiedad
+				FROM Recibo AS R
+				WHERE 1 = ALL(SELECT estado FROM Recibo WHERE IdPropiedad = R.IdPropiedad)
+				AND NOT EXISTS(SELECT * FROM Reconexion WHERE IdPropiedad = R.IdPropiedad)
+				AND EXISTS(SELECT * FROM Corte WHERE IdPropiedad = R.IdPropiedad)
+				AND (R.IdCCobro = 1 OR R.IdCCobro = 10)
+				SELECT @idMenor = MIN(id), @idMayor = MAX(id) FROM @idPropiedades
+				WHILE @idMenor<=@idMayor
+				BEGIN
+				SELECT @idc = MAX(ID) + 1 FROM Reconexion
+					SELECT @cant = (COUNT(*)) FROM Reconexion
+					IF  @cant = 0 
+						BEGIN
+							SELECT @idc=1
+						END
+
+				
+					INSERT INTO Reconexion(ID, IdPropiedad,IdReciboReconexion, Fecha)
+					SELECT @idc, idP.idPropiedad, R.id, @FechaActual
+					FROM @idPropiedades idP
+					INNER JOIN Recibo AS R ON R.IdPropiedad = idP.idPropiedad
+					WHERE idP.id = @idMenor AND R.IdCCobro = 10
+				SET @idMenor = @idMenor+1
+				END
+				
+			COMMIT
+		/*END TRY
+		BEGIN CATCH
+			If 
+			@@TRANCOUNT > 0 
+				ROLLBACK TRAN;
+			THROW 55004,'Error: No se ha podido reconectar',1;
+		END CATCH	*/
+	END
