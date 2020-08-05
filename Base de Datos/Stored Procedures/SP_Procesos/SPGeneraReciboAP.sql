7bfe6d011e22a0b8c7ab54050c0d8ca1431c8595
KevinFallas03 <48746116+KevinFallas03@users.noreply.github.com>
2020-08-05 02:57:52 -0600

    Merge Pago de Recibos y Pago de AP

--- Base de Datos/Stored Procedures/SP_Procesos/SPGeneraReciboAP.sql
+++ Base de Datos/Stored Procedures/SP_Procesos/SPGeneraReciboAP.sql
@@ -5,9 +5,9 @@ CREATE OR ALTER PROC [dbo].[spGeneraReciboAP]
 @FechaActual date
 AS 
 BEGIN
-	/*BEGIN TRY
+	BEGIN TRY
 		SET NOCOUNT ON 
-		SET XACT_ABORT ON*/
+		SET XACT_ABORT ON
 			DECLARE @dia int,
 			@interesm MONEY,
 			@montoamort MONEY,
@@ -17,28 +17,29 @@ BEGIN
 			@TasaInteresAnual FLOAT,
 			@plazoRestante INT,
 			@nuevoSaldo MONEY,
-			@idMov INT
-			DECLARE @aps TABLE (sec INT IDENTITY(1,1),idap int)
+			@idMov INT;
 
-			SET @TasaInteresAnual = CONVERT(decimal(5,2),(SELECT Valor FROM ValoresConfiguracion WHERE ID = 1))
-			SET @dia = DAY(@FechaActual)
+			DECLARE @aps TABLE (sec INT IDENTITY(1,1),idap int);
+
+			SET @TasaInteresAnual = CONVERT(decimal(5,2),(SELECT Valor FROM ValoresConfiguracion WHERE ID = 1));
+			SET @dia = DAY(@FechaActual);
 
 			--Todos los AP que generan recibos este día 
 			INSERT INTO @aps(idap)
 			SELECT id 
 			FROM ArregloPago
-			WHERE DAY(insertedAt) = @dia AND PlazoResta > 0
+			WHERE DAY(insertedAt) = @dia AND PlazoResta > 0;
 
-			SELECT @idMenor = MIN(sec), @idMayor = MAX(sec) FROM @aps
+			SELECT @idMenor = MIN(sec), @idMayor = MAX(sec) FROM @aps;
 
-			--BEGIN TRAN
+			  BEGIN TRAN
 				WHILE (@idMenor<=@idMayor)
 				BEGIN 
 					--Se obtienen los datos de intereses y monto de amortización
-					SElECT @interesm =  A.Saldo*(@TasaInteresAnual/12) / 100, @montoamort = A.Cuota
+					SElECT @interesm =  A.Saldo*(@TasaInteresAnual/12) / 100, @montoamort = A.Cuota - @interesm
 					FROM ArregloPago AS A
 					INNER JOIN @aps AS IA ON A.ID = IA.idap
-					WHERE IA.sec = @idMenor
+					WHERE IA.sec = @idMenor;
 					
 					--Se actualiza el saldo del AP
 					UPDATE ArregloPago
@@ -46,13 +47,13 @@ BEGIN
 					PlazoResta = PlazoResta-1
 					FROM ArregloPago AS AP 
 					INNER JOIN @aps AS IA ON AP.id = IA.idap
-					WHERE IA.sec = @idMenor
+					WHERE IA.sec = @idMenor;
 
 					--Se obtienen los datos del plazo restante y saldo nuevo
 					SELECT @plazoRestante = A.PlazoResta, @nuevoSaldo =  A.Saldo
 					FROM ArregloPago AS A
 					INNER JOIN @aps AS IA ON A.id = IA.idap
-					WHERE IA.sec = @idMenor 
+					WHERE IA.sec = @idMenor;
 
 					--Se crea el Movimiento del AP
 					INSERT INTO MovimientoAP (IdArregloPago,IdTipoMovAP,Monto,InteresesDelMes,plazoResta,nuevoSaldo,fecha,insertedAt)
@@ -60,7 +61,7 @@ BEGIN
 					FROM [dbo].[ArregloPago] AP
 					INNER JOIN @aps AS IA ON AP.id = IA.idap
 					WHERE IA.sec = @idMenor
-					SET @idMov = IDENT_CURRENT('[dbo].[MovimientosAP]')
+					SET @idMov = IDENT_CURRENT('[dbo].[MovimientoAP]');
 
 					--Se crea el recibo del AP
 					INSERT INTO Recibo (IdCCobro,Monto,Estado,IdPropiedad,FechaEmision,FechaMaximaPago)
@@ -69,23 +70,23 @@ BEGIN
 					INNER JOIN CCobro AS C ON C.ID = 12
 					INNER JOIN ArregloPago AS A ON A.ID = IA.idap
 					WHERE IA.sec = @idMenor
-					SET @idRecibo = IDENT_CURRENT('[dbo].[Recibos]')
-
+					SET @idRecibo = IDENT_CURRENT('[dbo].[Recibo]');
+					
 					INSERT INTO Recibo_AP(ID,Descripcion,IdMovimientoAP)
 					SELECT @idRecibo
 						,'Interes mensual:'+CAST(@interesm AS VARCHAR(30))
 						+', amortizacion:'+CAST(@montoamort AS VARCHAR(30))
 						+', plazo resta:'+CAST(@plazoRestante AS VARCHAR(30))
 						,@idMov
-					SET @idMenor += 1
+					SET @idMenor += 1;
 				END
 
-			/*COMMIT
+			COMMIT
 		END TRY
 		BEGIN CATCH
 			If @@TRANCOUNT > 0 
 				ROLLBACK TRAN;
 			THROW 50004,'Error: No se ha podido generar el recibo de AP',1;
-		END CATCH*/
+		END CATCH
 		return 0
 END
